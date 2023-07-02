d9b3b2d964c1c710f0721456f93af6626c2d0721
Eldad Shneor <eldadshneor@gmail.com>
2023-07-02 17:05:57 +0300

    chnaged backend url

--- src/hooks/useImage.js
+++ src/hooks/useImage.js
@@ -1,5 +1,5 @@
 
-const BASE_URL1 = "https://recipes-for-everyone-backend.onrender.com"
+const BASE_URL1 = "https://recipe-app-api-khxh.onrender.com"
 const BASE_URL='http://localhost:3500'
 const useImage = (ImageString) => {
     if(ImageString === null || ImageString === undefined)
