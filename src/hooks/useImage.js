8992bdc87e9a0d53ebc79723da4d3064f405c1ff
Eldad Shneor <eldadshneor@gmail.com>
2023-07-02 18:35:35 +0300

    Updated image

--- src/hooks/useImage.js
+++ src/hooks/useImage.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = "https://recipe-app-api-khxh.onrender.com"
+const BASE_URL = "https://recipe-app-api-khxh.onrender.com/"
 //const BASE_URL='http://localhost:3500'
 const useImage = (ImageString) => {
     if(ImageString === null || ImageString === undefined)
