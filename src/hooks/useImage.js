d1a9480c5ba25ff1fbb702e8b7e367ac6e7009a7
Eldad Shneor <eldadshneor@gmail.com>
2023-07-11 15:04:31 +0300

    Changed the Image viewer to work with cloudinary

--- src/hooks/useImage.js
+++ src/hooks/useImage.js
@@ -1,5 +1,5 @@
 const BASE_URL = "https://recipes-for-everyone-backend.onrender.com/";
-//  const BASE_URL = "http://localhost:3500";
+//const BASE_URL = "http://localhost:3500";
 
 /**
  * Custom hook for converting an image string to a full URL.
@@ -8,14 +8,18 @@ const BASE_URL = "https://recipes-for-everyone-backend.onrender.com/";
  * @param {string} ImageString - The image string or path to be converted.
  * @returns {string|undefined} The full URL of the image, or undefined if the input is null or undefined.
  */
-const useImage = (ImageString) => {
-  if (ImageString === null || ImageString === undefined) {
-    return undefined;
-  }
 
-  const dataImage = ImageString;
-  const dataURL = BASE_URL + '/' + dataImage.replace('uploads', '');
-  return dataURL;
-};
+// const oldUseImage = (ImageString) => {
+//   if (ImageString === null || ImageString === undefined) {
+//     return undefined;
+//   }
+
+//   const dataImage = ImageString;
+//   const dataURL = BASE_URL + '/' + dataImage.replace('uploads', '');
+//   return dataURL;
+// }
+const useImage = (ImageString) => {
+  return ImageString;
+}
 
 export default useImage;
